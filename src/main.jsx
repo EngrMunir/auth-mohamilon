48689f55cf299d67932d5028c4c69e1f2bc79168
Md Sirajul Munir <abc@gmail.com>
2023-10-28 11:01:22 +0600

    context api created

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Root/Root';
+import Home from './Home/Home';
+import Register from './Register/Register';
+import Login from './Header/Login';
+import AuthProvider from './Provider/AuthProvider';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>,
+      },
+      {
+        path:"/register",
+        element:<Register></Register>,
+      },
+      {
+        path:"/login",
+        element:<Login></Login>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
